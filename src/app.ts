613b3b3e73f12a3ff66b05974f09b50df7ba55c7
KozaX <55134615+KozaX@users.noreply.github.com>
2020-10-16 14:14:30 +0200

    add logic to display the template on the hosting node

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,40 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!; //2 different ways to do it
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = 'user-input'
+
+    this.titleInputElement = this.formElement.querySelector('#title')! as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector('#description')! as HTMLInputElement;
+    this.peopleInputElement = this.formElement.querySelector('#people')! as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+
+  private handleSubmission = (event: Event) => {
+    event.preventDefault();
+
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure = () => {
+    this.formElement.addEventListener('submit', this.handleSubmission);
+  }
+
+  private attach = () => {
+    this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
+  }
+}
+
+const project = new ProjectInput();
