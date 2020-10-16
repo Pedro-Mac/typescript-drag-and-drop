7732642da87de3e3032163d715014872704802d5
KozaX <55134615+KozaX@users.noreply.github.com>
2020-10-16 15:01:13 +0200

    add auto bind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+//autobind decorator
+const autobind = (_: any, _2: string, descriptor: PropertyDescriptor) => {
+  console.log(descriptor);
+  const originalMethod = descriptor.value; //the value of the descriptor is the method itself in this case
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      return originalMethod.bind(this);
+    }
+  }
+  return adjustedDescriptor;
+}
+
+//Project input class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -22,13 +36,27 @@ class ProjectInput {
   }
 
 
-  private handleSubmission = (event: Event) => {
-    event.preventDefault();
+  // private gatherUserInput(): [string, string, number] {
+  //   const enteredTitle = this.titleInputElement.value;
+  //   const enteredDecription = this.descriptionInputElement.value;
+  //   const enteredPeople = this.peopleInputElement.value;
 
+  //   if(enteredTitle.trim().length === 0 ||
+  //     enteredDecription.trim().length === 0 ||
+  //     enteredPeople.trim().length === 0) {
+
+  //     }
+  // }
+
+  @autobind
+  private handleSubmission(event: Event){
+    event.preventDefault();
+    // const userInput = this.gatherUserInput();
     console.log(this.titleInputElement.value);
   }
 
-  private configure = () => {
+
+  private configure() {
     this.formElement.addEventListener('submit', this.handleSubmission);
   }
 
